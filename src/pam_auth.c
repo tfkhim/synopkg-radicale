7c92eeb24488ee1b3cb59aa35794c2fdb3509531
Thomas Himmelstoß <tfkhim@users.noreply.github.com>
2017-10-25 20:53:30 +0200

    Changed the authentication utility.
    The program now uses the misc_conv function from the pam_misc library instead
    of a custom one.

--- src/pam_auth.c
+++ src/pam_auth.c
@@ -8,54 +8,8 @@
  *
  */
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-
 #include <security/pam_appl.h>
-
-int simple_conv(int num_msg,
-                const struct pam_message **msg,
-                struct pam_response **response,
-                void *data) {
-
-    if (num_msg <= 0 || num_msg > PAM_MAX_NUM_MSG) {
-        return PAM_CONV_ERR;
-    }
-
-    typedef struct pam_response* response_ptr;
-    response_ptr aresp = calloc(num_msg, sizeof(response_ptr));
-    if (aresp == NULL) {
-        return PAM_BUF_ERR;
-    }
-
-    const char* password = (const char*) data;
-    for (int i = 0; i < num_msg; i++) {
-        aresp[i].resp_retcode = 0;
-        aresp[i].resp = NULL;
-        if (msg[i]->msg_style == PAM_PROMPT_ECHO_OFF) {
-            aresp[i].resp = strdup(password);
-            if (aresp[i].resp == NULL) {
-                goto fail;
-            }
-        }
-    }
-
-    *response = aresp;
-    return PAM_SUCCESS;
-
-fail:
-    for (int i = 0; i < num_msg; i++) {
-        if (aresp[i].resp != NULL) {
-            memset(aresp[i].resp, 0, strlen(aresp[i].resp));
-            free(aresp[i].resp);
-        }
-    }
-
-    memset(aresp, 0, num_msg*sizeof(*aresp));
-    *response = NULL;
-    return PAM_CONV_ERR;
-}
+#include <security/pam_misc.h>
 
 int main(int argc, const char* argv[]) {
     if (argc < 2) {
@@ -63,13 +17,7 @@ int main(int argc, const char* argv[]) {
         return 1;
     }
 
-    char password[PAM_MAX_RESP_SIZE];
-    if (fgets(password, sizeof(password), stdin) == NULL) {
-        printf("Error: Could not read password from stdin");
-        return 1;
-    }
-
-    const struct pam_conv pam_conversation = {&simple_conv, (void*)password};
+    const struct pam_conv pam_conversation = {misc_conv, NULL};
 
     pam_handle_t* handle = NULL;
     int status = pam_start("login", argv[1], &pam_conversation, &handle);
