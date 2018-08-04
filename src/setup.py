dbdc5e1909740d2738bce6cc79ca0c1ce40bbea2
Thomas Himmelstoß <tfkhim@users.noreply.github.com>
2018-08-04 18:51:53 +0200

    Upate of the build environment.
    Changes:
     - Updated to virtualenv version 16.0.0
     - Fixed the virtualenv download mechanism
     - The integrity check of the virtualenv package now uses SHA256
     - Removed the obsolete radicale.conf file
     - The PAM authentication module now reads the radicale version from an
       environment variable and uses the same value as its own version

--- src/setup.py
+++ src/setup.py
@@ -7,14 +7,17 @@
 # This software may be freely distributed under the MIT license. You should
 # have received a copy of the MIT License along with this program.
 
+import os
 from setuptools import setup, find_packages
+
+radicaleVersion = os.environ["RADICALE_VERSION"]
 
 setup(
     name="radicale_pam_auth",
-    version="0.1.0",
+    version=radicaleVersion,
     description="A Radical authentication plugin using the linux PAM library",
     py_modules=["radicale_pam_auth"],
     packages=find_packages(),
-    install_requires=["radicale==2.1.8"],
+    install_requires=["radicale=={}".format(radicaleVersion)],
 )
 
